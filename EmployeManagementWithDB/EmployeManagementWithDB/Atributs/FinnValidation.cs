ca5b191992ff6f9f1a1965a7b42752caeb558dcf
Ceyhun <contact.ceyhun@gmail.com>
2022-11-20 16:28:14 +0400

    Atribut folder and FinnValidation atribute completed

---
+++ EmployeManagementWithDB/EmployeManagementWithDB/Atributs/FinnValidation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace EmployeManagementWithDB.Atributs
+{
+    public class FinnValidation : ValidationAttribute
+    {
+
+        public override bool IsValid(object? value)
+        {
+            var fin = value.ToString();
+
+            string regexed = @"^([A-Z0-9]{7}$)";
+
+            return Regex.IsMatch(fin, regexed);
+        
+
+        }
+    }
+}
