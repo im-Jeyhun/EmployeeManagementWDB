815c6f54b28c4e8f4da4ede7c1829b320a75decd
Ceyhun <contact.ceyhun@gmail.com>
2022-11-20 16:28:56 +0400

    Oher functionalies added.

---
+++ EmployeManagementWithDB/EmployeManagementWithDB/Program.cs
@@ -0,0 +1,23 @@
+namespace EmployeManagementWithDB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services
+                .AddMvc();
+                
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action}");
+
+            app.Run();
+        }
+    }
+}
